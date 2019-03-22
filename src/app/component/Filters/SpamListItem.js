aeb5bc42c128f789bb155983ec32201657cdec70
dhoko <aurelien@protonmail.com>
2019-03-22 18:36:46 +0100

    Setup filters

---
+++ src/app/component/Filters/SpamListItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { c } from 'ttag';
+import { SubTitle, Bordered, Button, Icon, PrimaryButton } from 'react-components';
+
+function SpamListItem({ list, type, dest, onClickMoveTo, onClickRemove }) {
+    const iconName = type === 'whitelist' ? `arrow-right` : `arrow-left`;
+    const I18N = {
+        whitelist: c('Title').t('Whitelist'),
+        blacklist: c('Title').t('BlackList')
+    };
+
+    return (
+        <Bordered className="flex-autogrid-item SpamListItem-container">
+            <header className="flex">
+                <SubTitle>{I18N[type]}</SubTitle>
+                <PrimaryButton className="mlauto">{c('Action').t('Add')}</PrimaryButton>
+            </header>
+            <ul className="unstyled scroll-if-needed SpamListItem-list p1">
+                {list.map((mail, i) => {
+                    return (
+                        <li className="flex" key={mail.ID}>
+                            <span className="ellipsis">{mail.Email}</span>
+                            <Button onClick={onClickMoveTo(dest, mail)} className="mlauto mr1">
+                                <Icon name={iconName} />
+                            </Button>
+                            <Button onClick={onClickRemove(mail)}>
+                                <Icon name="close" />
+                            </Button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </Bordered>
+    );
+}
+
+export default SpamListItem;
