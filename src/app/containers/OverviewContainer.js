35ec9dcfd45e906e4939ddb68b8eff6a84921470
Richard <richard@protonmail.com>
2019-04-09 10:25:31 +0200

    Add design for header

--- src/app/containers/OverviewContainer.js
+++ src/app/containers/OverviewContainer.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { chunk } from 'proton-shared/lib/helpers/array';
 import { Link } from 'react-router-dom';
 
 import pages from '../pages';
-
-const ITEM_PER_ROW = 4;
 
 const buildSections = ({ text, route, sections = [] }) => {
     const list = sections.length ? sections : [{ text, route }];
 
     return (
-        <ul>
+        <ul className="unstyled mt0-5">
             {list.map(({ text, route }) => {
                 return (
                     <li key={route}>
@@ -24,22 +21,18 @@ const buildSections = ({ text, route, sections = [] }) => {
 
 const OverviewContainer = () => {
     return (
-        <>
-            {chunk(pages, ITEM_PER_ROW).map((items, index) => {
+        <div className="settings-grid-container">
+            {pages.map(({ text, route, sections = [] }) => {
                 return (
-                    <div key={index.toString()} className="flex-autogrid">
-                        {items.map(({ text, route, sections }) => {
-                            return (
-                                <div key={route} className="flex-autogrid-item">
-                                    <h3>{text}</h3>
-                                    {buildSections({ text, route, sections })}
-                                </div>
-                            );
-                        })}
+                    <div key={route} className={`setting-grid ${sections.length > 4 ? 'setting-grid--tall' : ''}`}>
+                        <h2 className="h6 mb0-5">
+                            <strong>{text}</strong>
+                        </h2>
+                        {buildSections({ text, route, sections })}
                     </div>
                 );
             })}
-        </>
+        </div>
     );
 };
 
