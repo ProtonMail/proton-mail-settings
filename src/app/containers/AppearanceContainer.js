6db5fbe9445dad57f22d07e9515277af2e4e1abd
Richard <richard@protonmail.com>
2019-04-29 14:15:01 +0200

    Add toolbar

--- src/app/containers/AppearanceContainer.js
+++ src/app/containers/AppearanceContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { LayoutsSection, ThemesSection, ObserverSections } from 'react-components';
+import { LayoutsSection, ThemesSection, ObserverSections, ToolbarsSection } from 'react-components';
 
 const AppearanceContainer = () => {
     return (
         <ObserverSections>
             <LayoutsSection id="layouts" />
+            <ToolbarsSection id="toolbars" />
             <ThemesSection id="themes" />
         </ObserverSections>
     );
