16ceeae5c4915c5ed7afb1ae2c8fce68505fec0d
mmso <mmso@mmso.se>
2019-05-18 18:51:14 +0200

    Add ErrorBoundary to section

--- src/app/content/AuthenticatedAppRoutes.js
+++ src/app/content/AuthenticatedAppRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
+import { ErrorBoundary } from 'react-components';
 
 import AuthLayout from '../components/layout/AuthLayout.js';
 import OverviewContainer from '../containers/OverviewContainer';
@@ -17,7 +18,6 @@ import AppsContainer from '../containers/AppsContainer';
 import KeysContainer from '../containers/KeysContainer';
 import GeneralContainer from '../containers/GeneralContainer';
 import FiltersContainer from '../containers/FiltersContainer';
-import LabelsContainer from '../containers/Labels/Container';
 import AutoReplyContainer from '../containers/AutoReplyContainer';
 
 const NotFoundContainer = () => <h1>Not found</h1>;
@@ -26,25 +26,30 @@ const Routes = () => {
     return (
         <Router>
             <AuthLayout>
-                <Switch>
-                    <Route path="/settings" exact component={OverviewContainer} />
-                    <Route path="/settings/account" component={AccountContainer} />
-                    <Route path="/settings/organization" component={OrganizationContainer} />
-                    <Route path="/settings/members" component={MembersContainer} />
-                    <Route path="/settings/subscription" component={SubscriptionContainer} />
-                    <Route path="/settings/addresses" component={AddressesContainer} />
-                    <Route path="/settings/domains" component={DomainsContainer} />
-                    <Route path="/settings/identity" component={IdentityContainer} />
-                    <Route path="/settings/security" component={SecurityContainer} />
-                    <Route path="/settings/appearance" component={AppearanceContainer} />
-                    <Route path="/settings/keys" component={KeysContainer} />
-                    <Route path="/settings/apps" component={AppsContainer} />
-                    <Route path="/settings/general" component={GeneralContainer} />
-                    <Route path="/settings/labels" component={LabelsContainer} />
-                    <Route path="/settings/filters" component={FiltersContainer} />
-                    <Route path="/settings/auto-reply" component={AutoReplyContainer} />
-                    <Route component={NotFoundContainer} />
-                </Switch>
+                <Route
+                    render={({ location }) => (
+                        <ErrorBoundary key={location.key}>
+                            <Switch>
+                                <Route path="/settings" exact component={OverviewContainer} />
+                                <Route path="/settings/account" component={AccountContainer} />
+                                <Route path="/settings/organization" component={OrganizationContainer} />
+                                <Route path="/settings/members" component={MembersContainer} />
+                                <Route path="/settings/subscription" component={SubscriptionContainer} />
+                                <Route path="/settings/addresses" component={AddressesContainer} />
+                                <Route path="/settings/domains" component={DomainsContainer} />
+                                <Route path="/settings/identity" component={IdentityContainer} />
+                                <Route path="/settings/security" component={SecurityContainer} />
+                                <Route path="/settings/appearance" component={AppearanceContainer} />
+                                <Route path="/settings/keys" component={KeysContainer} />
+                                <Route path="/settings/apps" component={AppsContainer} />
+                                <Route path="/settings/general" component={GeneralContainer} />
+                                <Route path="/settings/filters" component={FiltersContainer} />
+                                <Route path="/settings/auto-reply" component={AutoReplyContainer} />
+                                <Route component={NotFoundContainer} />
+                            </Switch>
+                        </ErrorBoundary>
+                    )}
+                />
             </AuthLayout>
         </Router>
     );
