8f02b6c40574834116f71b85670020c7441df15d
Richard <richard@protonmail.com>
2019-05-20 14:57:16 +0200

    Adapt sidebar

--- src/app/components/layout/AuthLayout.js
+++ src/app/components/layout/AuthLayout.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Route, withRouter } from 'react-router';
-import { Sidebar, SubSidebar } from 'react-components';
+import { Sidebar, SubSidebar, useUser } from 'react-components';
 
 import pages from '../../pages';
 import AuthHeader from './AuthHeader';
 
 const AuthLayout = ({ children, history }) => {
+    const [{ hasPaidMail }] = useUser();
     const [subSidebarList, setSubSidebarList] = useState([]);
+    const list = pages
+        .filter(({ paidFeature }) => {
+            if (hasPaidMail) {
+                return true;
+            }
+            return !paidFeature;
+        })
+        .map(({ text, route: link, icon }) => ({ text, link, icon }));
 
     const buildSubSidebar = (location) => {
         const { sections = [] } = pages.find(({ route }) => location.pathname === route) || {};
@@ -31,10 +40,7 @@ const AuthLayout = ({ children, history }) => {
         <>
             <AuthHeader />
             <div className="flex flex-nowrap">
-                <Route
-                    path="/:path"
-                    render={() => <Sidebar list={pages.map(({ text, route: link, icon }) => ({ text, link, icon }))} />}
-                />
+                <Route path="/:path" render={() => <Sidebar list={list} />} />
                 <div className="main flex-item-fluid main-area">
                     <div className="flex flex-reverse">
                         {subSidebarList.length ? <SubSidebar list={subSidebarList} /> : null}
