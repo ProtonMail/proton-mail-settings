e1f2df46842d735c68f8cdfb1db843e6d2ac81ba
dhoko <aurelien+github@protonmail.com>
2019-06-03 17:35:22 +0200

    Load APP standalone

--- src/app/init.js
+++ src/app/init.js
@@ -6,10 +6,19 @@ import * as config from './config';
 
 const AuthenticatedAppRoutes = React.lazy(() => import('./content/AuthenticatedAppRoutes'));
 
-const UnAuthenticatedAppRoutes = () => {
-    document.location.replace(document.location.origin);
-    return 'Redirecting';
-};
+let UnAuthenticatedAppRoutes = () => {};
+
+if (!PL_IS_STANDALONE) {
+    UnAuthenticatedAppRoutes = () => {
+        document.location.replace(document.location.origin);
+        return 'Redirecting';
+    };
+}
+
+// usefull for dev/QA, else we don't need it when it's going to be live
+if (PL_IS_STANDALONE) {
+    UnAuthenticatedAppRoutes = React.lazy(() => import('./content/UnAuthenticatedAppRoutes'));
+}
 
 setConfig({
     ignoreSFC: true, // RHL will be __completely__ disabled for SFC
