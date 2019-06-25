c875a526298d6ce8f53d699a498c0c048fd8be9a
RandomAdversary <RandomAdversary@gmail.com>
2019-06-25 15:10:14 +0200

    Debug with @dhoko

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,18 +5,23 @@
 // ***********************************************
 
 import { login } from './commands/login.js';
-
-//Setup user data
-let users = require('../fixtures/accounts.json');
-let accountTypes = require('../fixtures/accountTypes.json');
+import { PLANS, PASSWORD_MODE } from '../fixtures/accountTypes.js';
+//let users = require('../fixtures/accounts.json');
+var path = require('path');
 
 /**
  * [Will perform login using one of the predefined test accounts.]
  * @param  {[String]} [type="free"] [The type of the account. Ex: free, visionary, etc.]
  * @param  {[String]} [mode="regular"] [The password mode. Can be: regular, 2FA, 2PasswordMode or 2FA2PasswordMode.]
  */
-Cypress.Commands.add('autoLogin', (type = accountTypes.type.free, mode = accountTypes.passwordMode.regular) => {
-    if (type == accountTypes.type.free && mode == accountTypes.passwordMode.twoFactorTwoPasswordMode) {
+Cypress.Commands.add('autoLogin', (plan = PLANS.free, passwordMode = PASSWORD_MODE.regular) => {
+    console.log(Cypress.env('testAccountsData'));
+    console.log(__dirname);
+    console.log('RESET');
+    console.log(path.join(process.cwd(), Cypress.env('testAccountsData')));
+    console.log(path.join(__dirname, Cypress.env('testAccountsData')));
+    const users = require(Cypress.env('testAccountsData'));
+    if (plan === PLANS.free && passwordMode === PASSWORD_MODE.twoFactorTwoPasswordMode) {
         return login(users.user2.username, users.user2.password, users.user2.OTP, users.user2.secondPassword);
     }
     return login(users.user1.username, users.user1.password);
@@ -26,9 +31,9 @@ Cypress.Commands.add('autoLogin', (type = accountTypes.type.free, mode = account
  * [Login command]
  * @param  {[String]} username [The username.]
  * @param  {[String]} password [The password.]
- * @param  {[String]} [OTP=null] [The secret for the OTP(one time password) token.]
- * @param  {[String]} [secondPassword=null] [The mailbox password.]
+ * @param  {[String]} OTP [The secret for the OTP(one time password) token.]
+ * @param  {[String]} secondPassword [The mailbox password.]
  */
-Cypress.Commands.add('login', (username, password, OTP = null, secondPassword = null) => {
+Cypress.Commands.add('login', (username, password, OTP, secondPassword) => {
     return login(username, password, OTP, secondPassword);
 });
--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,18 +1,18 @@
-const login = (username, password, OTP = null, secondPassword = null) => {
+export const login = (username, password, OTP, secondPassword) => {
     cy.visit('/');
     cy.url().should('include', '/login');
     cy.get('#username').type(username);
     cy.get('#password').type(password);
     cy.get('#login_btn').click();
 
-    if (OTP !== null) {
+    if (OTP) {
         cy.task('generateOTP', OTP).then((token) => {
             cy.get('.TwoFA-input').type(token);
             cy.get('#login_btn_2fa').click();
         });
     }
 
-    if (secondPassword !== null) {
+    if (secondPassword) {
         cy.get('input[name=mailbox-password]').type(secondPassword);
         cy.get('#unlock_btn').click();
     }
@@ -20,5 +20,3 @@ const login = (username, password, OTP = null, secondPassword = null) => {
     cy.get('.atomLoader-text').contains('Decrypting');
     return cy.url().should('include', '/inbox');
 };
-
-module.exports.login = login;
