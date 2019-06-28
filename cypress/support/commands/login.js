bb0c63d0b0059de966b4527838799254bb8f1b1a
RandomAdversary <RandomAdversary@gmail.com>
2019-06-28 17:30:24 +0200

    Refactor login

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,19 +1,19 @@
-export const login = (loginCredentials) => {
+export const login = ({ username, password, OTP, secondPassword }) => {
     cy.visit('/');
     cy.url().should('include', '/login');
-    cy.get('#username').type(loginCredentials.username);
-    cy.get('#password').type(loginCredentials.password);
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('#login_btn').click();
 
-    if (loginCredentials.OTP) {
-        cy.task('generateOTP', loginCredentials.OTP).then((token) => {
+    if (OTP) {
+        cy.task('generateOTP', OTP).then((token) => {
             cy.get('.TwoFA-input').type(token);
             cy.get('#login_btn_2fa').click();
         });
     }
 
-    if (loginCredentials.secondPassword) {
-        cy.get('input[name=mailbox-password]').type(loginCredentials.secondPassword);
+    if (secondPassword) {
+        cy.get('input[name=mailbox-password]').type(secondPassword);
         cy.get('#unlock_btn').click();
     }
 
