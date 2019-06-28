d75d76dbfbcaee6babd87b7721f9c03e7fd812c2
RandomAdversary <RandomAdversary@gmail.com>
2019-06-28 13:45:06 +0200

    Refactor the login command.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,23 +10,32 @@ const users = require('../fixtures/accounts.json');
 
 /**
  * [Will perform login using one of the predefined test accounts.]
- * @param  {[String]} [plan="free"] [The type of the account. Ex: free, visionary, etc.]
- * @param  {[String]} [passwordMode="regular"] [The password mode. Can be: regular, 2FA, 2PasswordMode or 2FA2PasswordMode.]
+ * @param  {String} [plan="free"] [The type of the account. Ex: free, visionary, etc.]
+ * @param  {String} [passwordMode="regular"] [The password mode. Can be: regular, 2FA, 2PasswordMode or 2FA2PasswordMode.]
  */
 Cypress.Commands.add('autoLogin', (plan = PLANS.free, passwordMode = PASSWORD_MODE.regular) => {
     if (plan === PLANS.free && passwordMode === PASSWORD_MODE.twoFactorTwoPasswordMode) {
-        return login(users.user2.username, users.user2.password, users.user2.OTP, users.user2.secondPassword);
+        return login({
+            username: users.user2.username,
+            password: users.user2.password,
+            OTP: users.user2.OTP,
+            secondPassword: users.user2.secondPassword
+        });
     }
-    return login(users.user1.username, users.user1.password);
+    return login({
+        username: users.user1.username,
+        password: users.user1.password
+    });
 });
 
 /**
  * [Login command]
- * @param  {[String]} username [The username.]
- * @param  {[String]} password [The password.]
- * @param  {[String]} OTP [The secret for the OTP(one time password) token.]
- * @param  {[String]} secondPassword [The mailbox password.]
+ * @param  {Object} loginCredentials [The login credentials fot the user.]
+ * @param  {String} loginCredentials.username [The username.]
+ * @param  {String} loginCredentials.password [The password.]
+ * @param  {String} loginCredentials.OTP [The secret for the OTP(one time password) token.]
+ * @param  {String} loginCredentials.secondPassword [The mailbox password.]
  */
-Cypress.Commands.add('login', (username, password, OTP, secondPassword) => {
-    return login(username, password, OTP, secondPassword);
+Cypress.Commands.add('login', (loginCredentials) => {
+    return login(loginCredentials);
 });
--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,19 +1,19 @@
-export const login = (username, password, OTP, secondPassword) => {
+export const login = (loginCredentials) => {
     cy.visit('/');
     cy.url().should('include', '/login');
-    cy.get('#username').type(username);
-    cy.get('#password').type(password);
+    cy.get('#username').type(loginCredentials.username);
+    cy.get('#password').type(loginCredentials.password);
     cy.get('#login_btn').click();
 
-    if (OTP) {
-        cy.task('generateOTP', OTP).then((token) => {
+    if (loginCredentials.OTP) {
+        cy.task('generateOTP', loginCredentials.OTP).then((token) => {
             cy.get('.TwoFA-input').type(token);
             cy.get('#login_btn_2fa').click();
         });
     }
 
-    if (secondPassword) {
-        cy.get('input[name=mailbox-password]').type(secondPassword);
+    if (loginCredentials.secondPassword) {
+        cy.get('input[name=mailbox-password]').type(loginCredentials.secondPassword);
         cy.get('#unlock_btn').click();
     }
 
