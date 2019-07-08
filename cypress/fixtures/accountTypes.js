039559f4182d4462a33e94c57c84e0c18f412025
RandomAdversary <RandomAdversary@gmail.com>
2019-07-08 12:01:33 +0200

    Add cypress.

---
+++ cypress/fixtures/accountTypes.js
@@ -0,0 +1,13 @@
+export const PLANS = {
+    free: 'free',
+    plus: 'plus',
+    professional: 'professional',
+    visionary: 'visionary'
+};
+
+export const PASSWORD_MODE = {
+    regular: 'regular',
+    twoFactorAuthentication: '2FA',
+    twoPasswordMode: '2PasswordMode',
+    twoFactorTwoPasswordMode: '2FA2PasswordMode'
+};
---
+++ cypress/support/commands.js
@@ -0,0 +1,42 @@
+// ***********************************************
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+import '@testing-library/cypress/add-commands';
+
+import { login } from './commands/login';
+import { PLANS, PASSWORD_MODE } from '../fixtures/accountTypes.js';
+const users = require('../fixtures/accounts.json');
+
+/**
+ * [Will perform login using one of the predefined test accounts.]
+ * @param  {String} [plan="free"] [The type of the account. Ex: free, visionary, etc.]
+ * @param  {String} [passwordMode="regular"] [The password mode. Can be: regular, 2FA, 2PasswordMode or 2FA2PasswordMode.]
+ */
+Cypress.Commands.add('autoLogin', (plan = PLANS.free, passwordMode = PASSWORD_MODE.regular) => {
+    if (plan === PLANS.free && passwordMode === PASSWORD_MODE.twoFactorTwoPasswordMode) {
+        return login({
+            username: users.FREE_TWOFACTOR_TWOPASSWORD_MODE.username,
+            password: users.FREE_TWOFACTOR_TWOPASSWORD_MODE.password,
+            OTP: users.FREE_TWOFACTOR_TWOPASSWORD_MODE.OTP,
+            secondPassword: users.FREE_TWOFACTOR_TWOPASSWORD_MODE.secondPassword
+        });
+    }
+    return login({
+        username: users.FREE.username,
+        password: users.FREE.password
+    });
+});
+
+/**
+ * [Login command]
+ * @param  {Object} loginCredentials [The login credentials for the user.]
+ * @param  {String} loginCredentials.username [The username.]
+ * @param  {String} loginCredentials.password [The password.]
+ * @param  {String} loginCredentials.OTP [The secret for the OTP(one time password) token.]
+ * @param  {String} loginCredentials.secondPassword [The mailbox password.]
+ */
+Cypress.Commands.add('login', (loginCredentials = { username, password, OTP, secondPassword }) => {
+    return login(loginCredentials);
+});
