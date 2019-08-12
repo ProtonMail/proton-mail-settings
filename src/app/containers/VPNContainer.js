7668b1ed9ec0c3aeccdae3cac7bf89797c25ff81
vincaslt <vincaslt@gmail.com>
2019-08-12 11:14:55 +0300

    Proton vpn page

--- src/app/containers/VPNContainer.js
+++ src/app/containers/VPNContainer.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { c } from 'ttag';
+import {
+    ProtonVPNClientsSection,
+    OpenVPNAccountSection,
+    ProtonVPNResourcesSection,
+    ProtonVPNCredentialsSection
+} from 'react-components';
+import Page from '../components/Page';
+
+// TODO: permissions?
+export const getVPNPage = () => {
+    return {
+        text: c('Title').t`ProtonVPN`,
+        route: '/settings/vpn',
+        icon: 'protonvpn',
+        sections: [
+            {
+                text: c('Title').t`ProtonVPN Clients`,
+                id: 'protonvpn-clients'
+            },
+            {
+                text: c('Title').t`ProtonVPN Credentials`,
+                id: 'protonvpn-credentials'
+            },
+            {
+                text: c('Title').t`OpenVPN / IKEv2 Username`,
+                id: 'openvpn-ikev2-username'
+            },
+            {
+                text: c('Title').t`ProtonVPN Resources`,
+                id: 'protonvpn-resources'
+            }
+        ]
+    };
+};
 
 const VPNContainer = () => {
-    return <>VPN Container</>;
+    return (
+        <Page config={getVPNPage()}>
+            <ProtonVPNClientsSection />
+            <ProtonVPNCredentialsSection />
+            <OpenVPNAccountSection />
+            <ProtonVPNResourcesSection />
+        </Page>
+    );
 };
 
 export default VPNContainer;
