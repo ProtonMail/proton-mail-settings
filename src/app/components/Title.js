3c01f7af6e8df06833b88b09f594d42ff4ca27ba
Richard <EpokK@users.noreply.github.com>
2019-08-19 11:31:38 +0200

    Fix #107 - Add listener on scroll to handle CSS class on Title (#109)
    * Fix #107 - Add listener on scroll to handle CSS class on Title
    
    * Review
    
    * Update src/app/components/Main.js
    
    Co-Authored-By: mmso <mmso@mmso.se>
    
    * Review
    
    * Provide main area div to Title component
    
    * Remove React.forwardRef

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { classnames } from 'react-components';
+
+import { useMainArea } from '../hooks/useMainArea';
 
 const Title = ({ children }) => {
-    return <h1 className="sticky-title sticky-title--onTop">{children}</h1>;
+    const mainAreaRef = useMainArea();
+    const [topClass, setClass] = useState('');
+
+    const onScroll = () => {
+        setClass(mainAreaRef.current.scrollTop ? '' : 'sticky-title--onTop');
+    };
+
+    useEffect(() => {
+        document.querySelector('.main-area').addEventListener('scroll', onScroll);
+        return () => {
+            mainAreaRef.current.removeEventListener('scroll', onScroll);
+        };
+    }, [mainAreaRef.current]);
+
+    return <h1 className={classnames(['sticky-title', topClass])}>{children}</h1>;
 };
 
 Title.propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    mainRef: PropTypes.object
 };
 
 export default Title;
