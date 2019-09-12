1a7c78b9cffb942201cf33b3ad118083e265659d
mmso <mmso@mmso.se>
2019-09-12 18:41:02 +0200

    Use standard public app

--- src/app/content/PublicApp.js
+++ src/app/content/PublicApp.js
@@ -1,42 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import { GenericError, LoaderPage, LoginForm, ModalsChildren } from 'react-components';
-import { loadOpenPGP } from 'proton-shared/lib/openpgp';
-import { loadLocale, getBrowserLocale } from 'proton-shared/lib/i18n';
+import { StandardPublicApp, LoginForm } from 'react-components';
 
 import locales from '../locales';
 import PublicLayout from '../components/layout/PublicLayout';
 
 const PublicApp = ({ onLogin }) => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-    useEffect(() => {
-        (async () => {
-            await Promise.all([loadOpenPGP(), loadLocale(getBrowserLocale(), locales)]);
-        })()
-            .then(() => setLoading(false))
-            .catch(() => setError(true));
-    }, []);
-
-    if (error) {
-        return <GenericError />;
-    }
-
-    if (loading) {
-        return <LoaderPage />;
-    }
-
     return (
-        <>
-            <ModalsChildren />
+        <StandardPublicApp locales={locales}>
             <PublicLayout>
                 <Switch>
                     <Route render={() => <LoginForm onLogin={onLogin} />} />
                 </Switch>
             </PublicLayout>
-        </>
+        </StandardPublicApp>
     );
 };
 
