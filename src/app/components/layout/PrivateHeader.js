0e3900d6be6fba51bb819fb58763f0a6df294634
Richard <EpokK@users.noreply.github.com>
2019-11-21 15:05:59 +0100

    Add  logic for the BlackFriday (#246)
    * Prepare logic for the BlackFriday
    
    * Add style import
    
    * Continue

--- src/app/components/layout/PrivateHeader.js
+++ src/app/components/layout/PrivateHeader.js
@@ -7,14 +7,18 @@ import {
     UpgradeButton,
     Hamburger,
     useUser,
-    useActiveBreakpoint
+    useActiveBreakpoint,
+    BlackFridayNavbarLink,
+    MailBlackFridayModal,
+    useBlackFriday
 } from 'react-components';
 import { c } from 'ttag';
 
-const PrivateHeader = ({ title, expanded, onToggleExpand }) => {
+const PrivateHeader = ({ title, location, expanded, onToggleExpand }) => {
     const [user = {}] = useUser();
     const { hasPaidMail } = user;
     const { isNarrow } = useActiveBreakpoint();
+    const isBlackFriday = useBlackFriday();
 
     return (
         <header className="header flex flex-items-center flex-nowrap reset4print">
@@ -22,7 +26,15 @@ const PrivateHeader = ({ title, expanded, onToggleExpand }) => {
             <Hamburger expanded={expanded} onToggle={onToggleExpand} />
             {title && isNarrow ? <span className="h3 mb0 ellipsis lh-standard">{title}</span> : null}
             <TopNavbar>
-                {hasPaidMail || isNarrow ? null : <UpgradeButton />}
+                {hasPaidMail || isNarrow ? null : isBlackFriday ? (
+                    <BlackFridayNavbarLink
+                        to="/settings/subscription"
+                        location={location}
+                        getModal={({ plans, onSelect }) => <MailBlackFridayModal plans={plans} onSelect={onSelect} />}
+                    />
+                ) : (
+                    <UpgradeButton />
+                )}
                 <TopNavbarLink to="/inbox" external={true} icon="mailbox" text={c('Title').t`Mailbox`} />
                 {isNarrow ? null : (
                     <TopNavbarLink
@@ -40,7 +52,8 @@ const PrivateHeader = ({ title, expanded, onToggleExpand }) => {
 PrivateHeader.propTypes = {
     title: PropTypes.string,
     expanded: PropTypes.bool,
-    onToggleExpand: PropTypes.func
+    onToggleExpand: PropTypes.func,
+    location: PropTypes.object.isRequired
 };
 
 export default PrivateHeader;
