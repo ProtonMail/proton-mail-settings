98bca4cff0665610ba3944dbb58310c8c18b0e6d
Yiin <stanislovas.janonis@gmail.com>
2019-12-05 17:31:45 +0200

    Pass upgrade button as a prop instead of children
    Related to ProtonMail/react-components#459

--- src/app/components/layout/PrivateLayout.js
+++ src/app/components/layout/PrivateLayout.js
@@ -69,11 +69,14 @@ const PrivateLayout = ({ location }) => {
         <div className="flex flex-nowrap no-scroll">
             <AppsSidebar
                 items={[
-                    <StorageSpaceStatus key="storage">
-                        <Link to="/settings/subscription" className="pm-button pm-button--primary pm-button--small">
-                            {c('Action').t`Upgrade`}
-                        </Link>
-                    </StorageSpaceStatus>
+                    <StorageSpaceStatus
+                        key="storage"
+                        upgradeButton={
+                            <Link to="/settings/subscription" className="pm-button pm-button--primary pm-button--small">
+                                {c('Action').t`Upgrade`}
+                            </Link>
+                        }
+                    />
                 ]}
             />
             <div className="content flex-item-fluid reset4print">
