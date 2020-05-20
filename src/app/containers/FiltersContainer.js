bd7a020b8ad3beb36b2ac306550691db70feaeb8
Richard <richard@protonmail.com>
2020-05-20 15:22:13 +0200

    [MAILWEB-617] Add "related settings" section for filters page

--- src/app/containers/FiltersContainer.js
+++ src/app/containers/FiltersContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
+import { FiltersSection, SpamFiltersSection, RelatedSettingsSection } from 'react-components';
 
 import Page from '../components/Page';
-import { FiltersSection, SpamFiltersSection } from 'react-components';
 
 export const getFiltersPage = () => {
     return {
@@ -18,6 +18,11 @@ export const getFiltersPage = () => {
             {
                 text: c('Title').t`Spam filters`,
                 id: 'spam'
+            },
+            {
+                text: c('Title').t`Related settings`,
+                id: 'related-settings',
+                hide: true
             }
         ]
     };
@@ -28,6 +33,24 @@ const FiltersContainer = ({ setActiveSection }) => {
         <Page config={getFiltersPage()} setActiveSection={setActiveSection}>
             <FiltersSection />
             <SpamFiltersSection />
+            <RelatedSettingsSection
+                list={[
+                    {
+                        icon: 'folder-label',
+                        text: c('Info')
+                            .t`Go to Folders/labels settings if you want to create and manage mailbox folders and labels.`,
+                        link: c('Link').t`Folders/labels settings`,
+                        to: '/settings/labels'
+                    },
+                    {
+                        icon: 'auto-reply',
+                        text: c('Info')
+                            .t`Go to Auto-reply settings if you want to create and manage auto-replys and notify anyone who tries to send you a message while you are out of the office.`,
+                        link: c('Link').t`Auto-reply settings`,
+                        to: '/settings/auto-reply'
+                    }
+                ]}
+            />
         </Page>
     );
 };
