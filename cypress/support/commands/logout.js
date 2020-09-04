149c2aa396081971a773fdbc764448f12109f965
mmso <mmso@mmso.se>
2020-09-04 13:21:16 +0200

    Remove cypress

--- cypress/support/commands/logout.js
+++
@@ -1,5 +0,0 @@
-export const logout = () => {
-    cy.get('[data-cy-header="userDropdown"]').click();
-    cy.get('[data-cy-header-user-dropdown="logout"]').click();
-    return cy.get('[data-cy-login="username"]').should('exist');
-};
--- cypress/support/helpers/clickAll.js
+++
@@ -1,5 +0,0 @@
-export const clickAll = (selectors) => {
-    selectors.forEach((selector) => {
-        cy.get(selector).click();
-    });
-};
